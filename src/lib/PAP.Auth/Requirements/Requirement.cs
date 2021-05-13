65a3282d1dff8a6b3ed93ad71db3bd4847c24443
arminatwork <workwitharmin7@gmail.com>
2021-05-14 02:24:15 +0430

    feat added global requirement

---
+++ src/lib/PAP.Auth/Requirements/Requirement.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace PAP.Auth.Requirements
+{
+    public record Requirement<TKey>(TKey Name) : IAuthorizationRequirement;
+
+    public record Requirement(string Name) : Requirement<string>(Name);
+}
