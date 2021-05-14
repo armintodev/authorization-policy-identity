629fbb04ae33414b5b79af616d618e0781c7ea79
arminatwork <workwitharmin7@gmail.com>
2021-05-15 01:58:27 +0430

    feat added permission policy provider

---
+++ src/lib/PAP.Auth/Authorization/PermissionPolicyProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+using PAP.Auth.Requirements;
+
+namespace PAP.Auth.Authorization
+{
+    internal class PermissionPolicyProvider : IAuthorizationPolicyProvider
+    {
+        public DefaultAuthorizationPolicyProvider FallbackPolicyProvider { get; set; }
+        public PermissionPolicyProvider(IOptions<AuthorizationOptions> options)
+        {
+            FallbackPolicyProvider = new DefaultAuthorizationPolicyProvider(options);
+        }
+
+        public virtual Task<AuthorizationPolicy> GetPolicyAsync(string policyName)
+        {
+            if (policyName.StartsWith(typeof(Requirement).GetProperty("Name")?.GetValue(null)?.ToString()!, StringComparison.OrdinalIgnoreCase))
+            {
+                var policy = new AuthorizationPolicyBuilder();
+                policy.AddRequirements(new PermissionRequirement(policyName));
+                return Task.FromResult(policy.Build());
+            }
+
+            return FallbackPolicyProvider.GetPolicyAsync(policyName);
+        }
+
+        public virtual Task<AuthorizationPolicy> GetDefaultPolicyAsync()
+        {
+            return FallbackPolicyProvider.GetDefaultPolicyAsync();
+        }
+
+        public virtual Task<AuthorizationPolicy> GetFallbackPolicyAsync()
+        {
+            return FallbackPolicyProvider.GetDefaultPolicyAsync();
+        }
+    }
+}
