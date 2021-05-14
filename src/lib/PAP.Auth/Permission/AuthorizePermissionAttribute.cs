4bae496c875c8d20db0cb36659ece9d796b6c544
arminatwork <workwitharmin7@gmail.com>
2021-05-15 01:57:07 +0430

    feat added authorize permission attribute

---
+++ src/lib/PAP.Auth/Permission/AuthorizePermissionAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PAP.Auth.Permission
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class AuthorizePermissionAttribute : Attribute
+    {
+        public string Permission { get; set; }
+        public AuthorizePermissionAttribute(string permission)
+        {
+            Permission = permission;
+        }
+
+    }
+}
