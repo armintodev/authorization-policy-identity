53dab275ee1faba073d0558cdfc02bcb974e98a2
arminatwork <workwitharmin7@gmail.com>
2021-05-15 02:02:11 +0430

    feat added controller type

---
+++ src/lib/PAP.Auth/Application/Controllers/ControllerType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace PAP.Auth.Application.Controllers
+{
+    internal abstract record ControllerType : TypeAbstract
+    {
+        public abstract IEnumerable<Type> GetControllers();
+        public abstract IEnumerable<MethodInfo> GetActions();
+    }
+}
