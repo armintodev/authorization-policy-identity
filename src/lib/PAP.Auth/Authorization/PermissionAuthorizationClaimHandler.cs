7cd61b19b9bd2ee8a66fcde5a649c5ae7074fac6
arminatwork <workwitharmin7@gmail.com>
2021-05-15 01:57:58 +0430

    feat added permission policy claim handler

---
+++ src/lib/PAP.Auth/Authorization/PermissionAuthorizationClaimHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using PAP.Auth.Requirements;
+
+namespace PAP.Auth.Authorization
+{
+    internal class PermissionAuthorizationClaimHandler : AuthorizationHandler<Requirement>
+    {
+        public PermissionAuthorizationClaimHandler() { }
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, Requirement requirement)
+        {
+            if (context?.User is null)
+            {
+                return;
+            }
+            var requirementClaims = context.User.Claims.Where(x =>
+                 x.Type == typeof(Requirement).GetProperty("Name")?.GetValue(null)?.ToString() &&
+                 x.Value == requirement.Name
+                 );
+
+            if (requirementClaims.Any())
+            {
+                context.Succeed(requirement);
+                return;
+            }
+        }
+    }
+}
