ffed55ae907dc5eaf8a50345c5685ce0a57bb30e
arminatwork <workwitharmin7@gmail.com>
2021-05-15 02:03:15 +0430

    feat added page type factory

---
+++ src/lib/PAP.Auth/Application/RazorPages/PageType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace PAP.Auth.Application.RazorPages
+{
+    internal abstract record PageType : TypeAbstract
+    {
+        public abstract IEnumerable<Type> GetPages();
+        public abstract IEnumerable<MethodInfo> GetHandlers();
+    }
+}
