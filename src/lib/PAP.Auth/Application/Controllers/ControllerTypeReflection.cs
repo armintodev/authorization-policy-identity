eb8bf6c30a665cf84d6d58d9273cf393ff17d1e1
arminatwork <workwitharmin7@gmail.com>
2021-05-15 02:02:34 +0430

    feat added controller type by reflection behavior

---
+++ src/lib/PAP.Auth/Application/Controllers/ControllerTypeReflection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PAP.Auth.Application.Controllers
+{
+    internal record ControllerTypeReflection : ControllerType
+    {
+        public override IEnumerable<Type> GetControllers()
+        {
+            return GetAssembly().GetTypes()
+                .Where(type => typeof(Controller).IsAssignableFrom(type) || typeof(ControllerBase).IsAssignableFrom(type));
+        }
+
+        public override IEnumerable<MethodInfo> GetActions()
+        {
+            return GetControllers().SelectMany(type => type.GetMethods())
+                .Where(method => method.IsPublic && !method.IsDefined(typeof(NonActionAttribute))/* &&
+                                 method.IsDefined(typeof(AuthorizePermissionAttribute))*/);
+        }
+    }
+}
