b364924c3839d0a66c5fa11c45d1af22f0541f7c
arminatwork <workwitharmin7@gmail.com>
2021-05-15 02:02:56 +0430

    feat added razor page type by reflection behavior

---
+++ src/lib/PAP.Auth/Application/RazorPages/RazorPageTypeReflection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PAP.Auth.Application.RazorPages
+{
+    internal record RazorPageTypeReflection : PageType
+    {
+        public override IEnumerable<Type> GetPages()
+        {
+            return GetAssembly().GetTypes()
+                .Where(type => typeof(PageModel).IsAssignableFrom(type));
+        }
+
+        public override IEnumerable<MethodInfo> GetHandlers()
+        {
+            return GetPages().SelectMany(type => type.GetMethods())
+                .Where(method => method.IsPublic && !method.IsDefined(typeof(NonHandlerAttribute))/* &&
+                                 method.IsDefined(typeof(AuthorizePermissionAttribute))*/);
+        }
+    }
+}
